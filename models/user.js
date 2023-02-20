d8717305186582ff525bc0cd3ab1cd653d54c5b7
Zuber8960 <zuberahmad8960@gmail.com>
2023-02-20 19:07:13 +0530

    order

--- models/user.js
+++ models/user.js
@@ -79,6 +79,35 @@ class User {
 
   }
 
+  addOrder(){
+    const db = getDb();
+    return this.getCart()
+      .then(products => {
+        const order = {
+          items: products,
+          user: {
+            _id: new ObjectId(this._id),
+            name : this.name,
+          }
+        }
+        return db.collection('order').insertOne(order);
+      })
+      .then(result => {
+        this.cart = {items : []};
+        return db
+          .collection('users')
+          .updateOne(
+            { _id : new ObjectId(this._id)} ,
+            { $set: {cart : {items: []}} }
+          );
+      })
+  }
+
+  getOrders(){
+    const db = getDb();
+    return db.collection('order').find({"user._id" : new ObjectId(this._id)}).toArray();
+  }
+
   static findById(userId){
     const db = getDb();
     return db.collection('users').findOne({_id: new ObjectId(userId)})

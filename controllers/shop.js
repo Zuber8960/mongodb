54f15f068cb6040f509b240b244e4365b53a83e7
Zuber8960 <zuberahmad8960@gmail.com>
2023-02-20 17:52:14 +0530

    delete_item_from_cart

--- controllers/shop.js
+++ controllers/shop.js
@@ -71,7 +71,8 @@ exports.postCart = (req, res, next) => {
   })
   .then((result) => {
     console.log('product added to cart');
-    console.log(result);
+    // console.log(result);
+    return res.redirect('/cart');
   })
   .catch(err => console.log(err))
   // let fetchedCart;
@@ -109,15 +110,8 @@ exports.postCart = (req, res, next) => {
 exports.postCartDeleteProduct = (req, res, next) => {
   const prodId = req.body.productId;
   req.user
-    .getCart()
-    .then(cart => {
-      return cart.getProducts({ where: { id: prodId } });
-    })
-    .then(products => {
-      const product = products[0];
-      return product.cartItem.destroy();
-    })
-    .then(result => {
+    .deleteItemFromCart(prodId)
+    .then(() => {
       res.redirect('/cart');
     })
     .catch(err => console.log(err));
